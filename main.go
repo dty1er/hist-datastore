e67498ee5c612c92c9d9bedc57613892f5a1d599
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2018-12-27 15:56:07 +0900

    update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
@@ -15,7 +16,11 @@ const (
 	projectID = "hist-datastore"
 )
 
-var client *datastore.Client
+// DataStore ...
+type DataStore interface {
+	Get(ctx context.Context, pwd string)
+	Put(ctx context.Context, pwd, cmd string)
+}
 
 // History represents the kind of Datastore
 type History struct {
@@ -24,43 +29,95 @@ type History struct {
 	Timestamp time.Time
 }
 
-func init() {
-	c, err := datastore.NewClient(context.Background(), projectID)
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-	client = c
+// Client ...
+type Client struct {
+	ds *datastore.Client
 }
 
 func main() {
 	ctx := context.Background()
+
+	c, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	cl := &Client{ds: c}
+
 	op, pwd, cmd := os.Args[1], os.Args[2], os.Args[3:]
+
 	if op != "put" && op != "get" {
 		log.Fatalf("specify get or put")
 	}
 
+	// Put
 	if op == "put" {
-		put(ctx, pwd, strings.Join(cmd, " "))
+		go func() {
+			if err := cl.Put(ctx, pwd, strings.Join(cmd, " ")); err != nil {
+				log.Fatalf("Failed to save history: %v", err)
+			}
+		}()
 		return
 	}
-	get(ctx, pwd)
+
+	// Get
+	var hists []History
+	hists, err = getFromCache(pwd)
+	if err != nil {
+		log.Printf("Failed to get from cache: %v", err)
+
+		hists, err = cl.Get(ctx, pwd)
+		if err != nil {
+			log.Printf("Failed to get history: %v", err)
+		}
+	}
+	printHists(hists)
 }
 
-func get(ctx context.Context, pwd string) {
-	var hists []History
-	query := datastore.NewQuery("History").Filter("Pwd = ", pwd).Limit(100)
-	_, err := client.GetAll(ctx, query, &hists)
-	if err != nil {
-		log.Fatalf("Failed to get history: %v", err)
-	}
+func printHists(hists []History) {
 	for _, hist := range hists {
 		fmt.Println(hist.Command)
 	}
 }
 
-func put(ctx context.Context, pwd, cmd string) {
-	if _, err := client.Put(ctx, datastore.IncompleteKey("History", nil),
-		&History{Timestamp: time.Now(), Pwd: pwd, Command: cmd}); err != nil {
-		log.Fatalf("Failed to save history: %v", err)
+func getFromCache(pwd string) (hists []History, err error) {
+	cacheFileName := "hist-datastore"
+	cache := fmt.Sprintf("%s/.cache/%s", os.Getenv("HOME"), cacheFileName)
+
+	xdgCacheHome := os.Getenv("XDG_CACHE_HOME")
+	if xdgCacheHome != "" {
+		cache = fmt.Sprintf("%s/%s", xdgCacheHome, cacheFileName)
 	}
+	cacheFile, err := os.Open(cache)
+	if err != nil {
+		return
+	}
+	cacheBytes, err := ioutil.ReadAll(cacheFile)
+	if err != nil {
+		return
+	}
+
+	// cache file format
+	//
+
+	return nil, nil
+}
+
+// Get ...
+func (cl *Client) Get(ctx context.Context, pwd string) (hists []History, err error) {
+	query := datastore.NewQuery("History").Filter("Pwd = ", pwd).Limit(100)
+	_, err = cl.ds.GetAll(ctx, query, &hists)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// Put ...
+func (cl *Client) Put(ctx context.Context, pwd, cmd string) error {
+	key := datastore.IncompleteKey("History", nil)
+	hist := &History{Timestamp: time.Now(), Pwd: pwd, Command: cmd}
+	if _, err := cl.ds.Put(ctx, key, hist); err != nil {
+		return err
+	}
+	return nil
 }
